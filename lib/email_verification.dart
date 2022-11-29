54bc989762f002c91c430ae2fa29a02b56970659
Muhammad Tayyab <mt6780175@gmail.com>
2022-11-29 20:57:57 +0500

    Email Verification page Half

---
+++ lib/email_verification.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Email_Verification extends StatelessWidget {
+  const Email_Verification({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/backarrow.png'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                        fontFamily: 'Avenir Roman',
+                        fontSize: 32,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 17.0,
+              ),
+              Row(
+                children: [
+                  SizedBox(height: 17.0),
+                  Text(
+                    "Verification",
+                    style: TextStyle(
+                        fontFamily: 'Avenir Roman',
+                        fontSize: 32,
+                        color: Color.fromRGBO(43, 147, 72, 1)),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 17.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "A verification code has been sent on",
+                    style: TextStyle(fontSize: 18, fontFamily: "Avenir Roman"),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "your email",
+                    style: TextStyle(fontSize: 18, fontFamily: "Avenir Roman"),
+                  ),
+                  Text(
+                    " saad@ahmedgraf.com",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "Avenir Roman",
+                        color: Color.fromRGBO(43, 147, 72, 1)),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
